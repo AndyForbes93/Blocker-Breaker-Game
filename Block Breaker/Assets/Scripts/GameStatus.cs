0be4148b1ef4177e618b265e97ddc4af19482da8
Andy Forbes <aforbes@knights.ucf.edu>
2018-08-01 02:05:01 -0400

    Added Score counter

--- Block Breaker/Assets/Scripts/GameStatus.cs
+++ Block Breaker/Assets/Scripts/GameStatus.cs
@@ -1,20 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameStatus : MonoBehaviour {
-    //params
-    [Range(0.1f , 10f)] [SerializeField] float gameSpeed = 1f;
-    [SerializeField] int pointsPerBlock = 69;
+	//params
+	[Range(0.1f , 3f)] [SerializeField] float gameSpeed = 1f;
+	[SerializeField] int pointsPerBlock = 69;
+	[SerializeField] Text scoreText;
 
-    //state
-    [SerializeField] int currScore = 0;
+	//state
+	[SerializeField] int currScore = 0;
+
+	public void Awake()
+	{
+		int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+
+		if(gameStatusCount > 1)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			DontDestroyOnLoad(gameObject);
+		}
+	}
 
 
+	//displayin starting score 
+	public void Start()
+	{
+		scoreText.text = "Score: " + currScore.ToString(); 
+		
+	}
+
+	//adde=s broken blocks to score and displays
 	public void AddToScore()
-    {
-        currScore += pointsPerBlock;
-    }
+	{
+		currScore += pointsPerBlock;
+		scoreText.text = "Score: " + currScore.ToString();
+	}
 	
 	// Update is called once per frame
 	void Update () {
